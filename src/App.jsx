48fcd4a5c778538b7ffa3e9d54704272b3668bd2
VaNomad <sjur.hassel@gmail.com>
2023-05-28 14:32:12 +0100

    testing what gets logged with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,37 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 
 function App() {
   const [resource, setResource] = useState('posts');
+
+  console.log('render')
+
+  useEffect(() => {
+    console.log('resource type changed')
+  }, [resource])
 
   return (
     <>
       <div className="flex flex-col justify-center items-center h-screen text-4xl text-white bg-slate-700">
         <h1 className="text-fuchsia-500">
-          <span className="text-white">{"<"}</span> ResourceType <span className="text-white">{">"}</span>
+          <span className="text-white">{"<"}</span> ResourceType{" "}
+          <span className="text-white">{">"}</span>
         </h1>
         <div className="w-full flex justify-around p-5">
           <button
             onClick={() => setResource("posts")}
-            className="bg-slate-600 p-2 border"
+            className="bg-slate-600 p-2 border rounded-lg"
           >
             Posts
           </button>
           <button
             onClick={() => setResource("users")}
-            className="bg-slate-600 p-2 border"
+            className="bg-slate-600 p-2 border rounded-lg"
           >
             Users
           </button>
           <button
             onClick={() => setResource("comments")}
-            className="bg-slate-600 p-2 border"
+            className="bg-slate-600 p-2 border rounded-lg"
           >
             Comments
           </button>
