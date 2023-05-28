1dd97c7b01fa78c16aa58b1396c8b119941911fb
VaNomad <sjur.hassel@gmail.com>
2023-05-28 15:22:41 +0100

    style updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [resource, setResource] = useState('posts');
+  const [resource, setResource] = useState("posts");
   const [items, setItems] = useState([]);
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/${resource}`)
       .then((response) => response.json())
       .then((json) => setItems(json));
-  }, [resource])
+  }, [resource]);
 
   return (
     <>
       <div className="flex flex-col justify-center items-center h-screen text-4xl text-white bg-slate-700">
         <h1 className="text-fuchsia-500">
-          <span className="text-white">{"<"}</span> ResourceType{" "}
-          <span className="text-white">{">"}</span>
+          <span className="text-white">{"<"}</span>
+          ResourceType <span className="text-white">{">"}</span>
         </h1>
         <div className="w-full flex justify-around p-5">
           <button
@@ -38,12 +38,12 @@ function App() {
           </button>
         </div>
         <h1 className="mb-5">{resource}</h1>
-        <p className="overflow-scroll text-sm flex flex-col p-6">
-          {items.map((item) => {
+        <div className="text-sm flex flex-col w-5/6 overflow-scroll">
+          { items.map((item) => {
             // eslint-disable-next-line react/jsx-key
-            return <p className="p-2">{JSON.stringify(item)}</p>;
+            return <p className="p-3">{ JSON.stringify(item) }</p>;
           })}
-        </p>
+        </div>
       </div>
     </>
   );
