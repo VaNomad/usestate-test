200e8f34b964814ed63b6f9c1ee5fda338a281b1
VaNomad <sjur.hassel@gmail.com>
2023-05-28 14:22:32 +0100

    style update

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,28 @@ function App() {
   return (
     <>
       <div className="flex flex-col justify-center items-center h-screen text-4xl text-white bg-slate-700">
-        <h1>ResourceType</h1>
+        <h1 className="text-fuchsia-500">
+          <span className="text-white">{"<"}</span> ResourceType <span className="text-white">{">"}</span>
+        </h1>
         <div className="w-full flex justify-around p-5">
-          <button onClick={() => setResource('posts')} className="bg-slate-600 p-2 border">Posts</button>
-          <button onClick={() => setResource('users')} className="bg-slate-600 p-2 border">Users</button>
-          <button onClick={() => setResource('comments')} className="bg-slate-600 p-2 border">Comments</button>
+          <button
+            onClick={() => setResource("posts")}
+            className="bg-slate-600 p-2 border"
+          >
+            Posts
+          </button>
+          <button
+            onClick={() => setResource("users")}
+            className="bg-slate-600 p-2 border"
+          >
+            Users
+          </button>
+          <button
+            onClick={() => setResource("comments")}
+            className="bg-slate-600 p-2 border"
+          >
+            Comments
+          </button>
         </div>
         <h1>{resource}</h1>
       </div>
